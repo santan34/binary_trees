ab34364e65577c9410f40fdf899388ba4678aa0a
St <takundaveni@gmail.com>
2024-03-27 15:10:08 -0700

    123

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 /**
-* binary_tree_insert_left - creates a binary tree node
-* @parent : pointer to the parent node of the node to insert to
-* @value : value to put in the new node
-* Return: pointer to the new node, or NULL on failure
-*/
+ * binary_tree_insert_left - creates a binary tree node
+ * @parent : pointer to the parent node of the node to insert to
+ * @value : value to put in the new node
+ * Return: pointer to the new node, or NULL on failure
+ */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
